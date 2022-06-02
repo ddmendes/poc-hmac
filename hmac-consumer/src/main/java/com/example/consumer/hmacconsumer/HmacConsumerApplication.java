748ad01f765d9f625bf0db1d11994df7c181cfe7
Davi Diorio Mendes <d.mendes@samsung.com>
2022-06-02 11:06:35 -0300

    Template projects

---
+++ hmac-consumer/src/main/java/com/example/consumer/hmacconsumer/HmacConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumer.hmacconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmacConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmacConsumerApplication.class, args);
+	}
+
+}
---
+++ hmac-producer/src/main/java/com/example/producer/hmacproducer/HmacProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.producer.hmacproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmacProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmacProducerApplication.class, args);
+	}
+
+}
